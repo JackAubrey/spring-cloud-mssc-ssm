0bc075bfe47101698a469c06b2a6850545616ceb
danilo <d.cividin@hotmail.it>
2024-03-05 22:51:43 +0100

    first model layer configuration

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscssm.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState state;
+
+    private BigDecimal amount;
+}
