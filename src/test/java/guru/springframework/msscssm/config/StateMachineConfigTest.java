d1e5662763e3b0cfb96c9649cc06b1738b055674
danilo <d.cividin@hotmail.it>
2024-03-11 21:26:05 +0100

    fixed test

--- src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -13,6 +13,8 @@ import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class StateMachineConfigTest {
     @Autowired
@@ -24,13 +26,16 @@ class StateMachineConfigTest {
         sm.startReactively().subscribe();
 
         System.out.println("State Machine State: " + sm.getState().getId());
+        assertEquals(PaymentState.NEW, sm.getState().getId());
 
         Message<PaymentEvent> event = MessageBuilder.withPayload(PaymentEvent.PRE_AUTHORIZE).build();
         sm.sendEvent(Mono.just(event)).subscribe();
         System.out.println("State Machine State: " + sm.getState().getId());
+        assertEquals(PaymentState.NEW, sm.getState().getId());
 
         Message<PaymentEvent> event2 = MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_APPROVED).build();
         sm.sendEvent(Mono.just(event2)).subscribe();
         System.out.println("State Machine State: " + sm.getState().getId());
+        assertEquals(PaymentState.PRE_AUTH, sm.getState().getId());
     }
 }
